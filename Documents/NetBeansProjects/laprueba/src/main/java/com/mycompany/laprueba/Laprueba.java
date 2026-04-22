520d33ecca68cb623c72ca31a144ac599a425996
Luna <lunaperera152@gmail.com>
2026-04-22 11:35:45 +0200

    el comit11

---
+++ Documents/NetBeansProjects/laprueba/src/main/java/com/mycompany/laprueba/Laprueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laprueba;
+
+/**
+ *
+ * @author Luna
+ */
+public class Laprueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
