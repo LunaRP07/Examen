3949a4f0382ed7d65fab4d4298fb8dd8648ea6a6
Luna <lunaperera152@gmail.com>
2026-04-22 09:59:35 +0200

    primer cooomit1

---
+++ Documents/NetBeansProjects/mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Luna
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        System.out.println("hola!");
+    }
+}
